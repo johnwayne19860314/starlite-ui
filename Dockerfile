9ffb385715029c46317ef7d983a0a0ec46cc57ce
jiangZhiJun <18101636214@163.com>
2024-01-08 18:04:35 +0800

    setup

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM nginx:1.21-alpine
 
 # 复制构建的前端文件到 nginx 容器中
 COPY dist/ /usr/share/nginx/html
-
+COPY nginx/default.conf /etc/nginx/conf.d/default.conf
+COPY nginx/nginx.conf /etc/nginx/nginx.conf
 # 暴露端口
 EXPOSE 80
 
--- config/proxy.ts
+++ config/proxy.ts
@@ -1,7 +1,9 @@
 const proxy = {
   '/api': {
-    target: 'http://starlite-first.ns-xx7juor7.svc.cluster.local:8080',
-    //target: 'http://127.0.0.1:8080',
+    // target: 'http://starlite-first.ns-xx7juor7.svc.cluster.local:8080',
+    // target: 'https://kdwqevbfhfcv.cloud.sealos.io',
+    target: 'http://127.0.0.1:8080',
+    
     changeOrigin: true,
     pathRewrite: { '^/api': '/api/starlite' },
   }
