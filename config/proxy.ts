d7cc46d3e03f4344c30cf5f0dfde944d9308311d
jiangZhiJun <18101636214@163.com>
2024-01-08 16:28:31 +0800

    setup

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,6 +1,7 @@
 const proxy = {
   '/api': {
     target: 'http://starlite-first.ns-xx7juor7.svc.cluster.local:8080',
+    //target: 'http://127.0.0.1:8080',
     changeOrigin: true,
     pathRewrite: { '^/api': '/api/starlite' },
   }
